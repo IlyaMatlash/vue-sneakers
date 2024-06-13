e965a22c0bc8a90f255a9bbb73988f9f4e447e68
Ilya Matlash <ilyamatlash123@gmail.com>
2024-06-13 19:02:18 +0300

    new component Card

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="relative m-10 border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+        <img class="absolute top-8 left-8" src="/like-2.svg" alt="Like default"/>
+        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker"/>
+        <p class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</p>
+
+        <div class="flex justify-between mt-5">
+          <div class="flex flex-col">
+            <span class="text-slate-400">Цена:</span>
+            <b>12 999 руб.</b>
+          </div>
+          <img src="/plus.svg" alt="Plus"/>
+        </div>
+      </div>
+</template>
