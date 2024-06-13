cc492d3d4e6a43ec3647a1305be12b4186a26342
Ilya Matlash <ilyamatlash123@gmail.com>
2024-06-13 19:30:46 +0300

    main grid

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,18 @@ import Card from './components/Card.vue'
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
-    <div>
-      <Card />
-      <Card />
-      <Card />
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="grid grid-cols-4 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   </div>
 </template>
