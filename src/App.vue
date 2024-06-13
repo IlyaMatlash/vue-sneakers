5b2d2e8319dd867f696813c3302d76808a20dc4e
Ilya Matlash <ilyamatlash123@gmail.com>
2024-06-13 17:42:16 +0300

    Creating a project . Using the Tailwind library

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <h1>Vue 3 app</h1>
+  </div>
+  
+</template>
+
+<style scoped></style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+    <h3>Добавлен: {{ isAdded }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.product {
+  border: 1px solid black;
+  border-radius: 16px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
