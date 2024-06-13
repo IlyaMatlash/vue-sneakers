ddd7e6fa2707d74461f011db375dd08b3839af52
Ilya Matlash <ilyamatlash123@gmail.com>
2024-06-13 20:30:52 +0300

    Displaying information on a card. Click events

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,16 +1,20 @@
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {
+    alert('Добавлено в корзину')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card 
+    title="Мужские Кроссовки Nike Blazer Mid Suede" 
+    image-url="/sneakers/sneakers-1.jpg" 
+    :price="5000"
+    @click-add="onClickAdd" 
+    :is-added="false" 
+    :is-favorite="true"
+    />
   </div>
 </template>
